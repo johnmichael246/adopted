94f6bcbec2d85dc0093a19689cd1ebef6a862e79
Jess Bakkers <jessica.bakkers@gmail.com>
2017-08-12 23:56:56 -0700

    made alternate petCtrl for search, dummy search bar for testing - do not merge these changes

--- controllers/apiCtrl.js
+++ controllers/apiCtrl.js
@@ -1,1 +1,2 @@
 var router = require('express').Router();
+
--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,8 @@
 var router = require('express').Router();
+var petFinderCtrl = require('../controllers/petFinderCtrl');
 
+
+router.get('/search', petFinderCtrl.search);
 
 
 module.exports= router;
