f9e292676b7656cb44b93350b3aa2c29093df3e2
Jess Bakkers <jessica.bakkers@gmail.com>
2017-08-14 14:37:32 -0700

    added underscore.js and added script to footer.ejs

---
+++ public/javascripts/underscore.js
@@ -0,0 +1,11 @@
+console.log('underscore.js has loaded');
+
+$(document).ready(function() {
+  console.log('document loaded! yay!');
+
+
+  // var renderPet = _.template(document.getElementById('petTemplate').innerHTML);
+
+
+
+});
