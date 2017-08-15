705c191a3b0d62d4f9f71f3de0637a653cc3c8e5
john solis <1johnmichael.solis@gmail.com>
2017-08-15 11:57:03 -0700

    working on search page

--- controllers/petsCtrl.js
+++ controllers/petsCtrl.js
@@ -10,14 +10,13 @@ function search(req,res,next) {
         url: `${basePath}pet.find?&key=${process.env.PETFINDER_KEY}&secret=${process.env.PETFINDER_SECRET}&format=json&size=${req.body.size}&age=${req.body.age}&animal=${req.body.animal}&location=${req.body.zip}&count=27`,
         method: 'GET'
     };
-    console.log(options.url)
     request(options.url, function(err,response,body) {
-        console.log('+++++++++++++++++++++')
         let doc = JSON.parse(body);
         // console.log(Object.keys(doc.petfinder))
         res.render('results', {doc});
     });
 }
+
 function show(req,res,next) {
      var options = {
         url: `${basePath}pet.get?&key=${process.env.PETFINDER_KEY}&secret=${process.env.PETFINDER_SECRET}&format=json&id=${req.params.id}`,
@@ -29,14 +28,6 @@ function show(req,res,next) {
         res.render('showpet', {doc});
     });
 }
-
-// function getFavorite() {
-//   User.findById(req.user.id), function(err, user) {
-//       Pet.findById
-//   }
-// }
-
-
 
 
 
