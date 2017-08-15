28d737db388af24edd9a272fd2b2e0e1606b1a16
Jess Bakkers <jessica.bakkers@gmail.com>
2017-08-15 10:02:11 -0700

    heart button in results.ejs changes colors when selected

--- public/javascripts/results.js
+++ public/javascripts/results.js
@@ -3,23 +3,20 @@ console.log('loaded results.js');
 $(document).ready(function() {
   console.log('jQ ready now');
 
-$('button.favorite').click(function(e) {
+$('button.heart-button').click(function(evt) {
   var $this = $(this);
-  $this.toggleClass('favorite');
-  if($this.hasClass('favorite')){
-    $this.text('Favorite');
+  if($this.hasClass('heart-button')){
+    $(this).find('span.glyphicon-heart').css("color", "#FF0000");
+    $this.toggleClass('heart-button');
     console.log('favorite');
+    console.log(evt.target.id);
   } else {
-    $this.text('Unfavorite');
+    $(this).find('span.glyphicon-heart').css("color", "#000000");
+    $this.toggleClass('heart-button');
     console.log('unfavorite');
+    console.log(evt.target.id);
   }
-  console.log(e.target.id);
 })
-
-
-
-
-
 
 
 
