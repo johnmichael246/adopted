22e0fd3a89aeb4275eacdca35c44672a3dccbd7a
Jess Bakkers <jessica.bakkers@gmail.com>
2017-08-14 20:58:10 -0700

    added favorite button to results.ejs and jQ to log petid

--- public/javascripts/results.js
+++ public/javascripts/results.js
@@ -3,6 +3,13 @@ console.log('loaded results.js');
 $(document).ready(function() {
   console.log('jQ ready now');
 
+$('button.favorite').on('click', function(e) {
+  console.log(e.target.id);
+})
+
+
+
+
 
 
 })
