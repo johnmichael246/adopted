d42bd2382b004cf92900ef371f20453f89ed125f
john solis <1johnmichael.solis@gmail.com>
2017-08-14 18:16:08 -0700

    form is now working

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,16 +1,16 @@
 var router = require('express').Router();
 var request = require('request');
-require('./../models/user');
-const basePath = "http://api.petfinder.com/"
+var User = require('./../models/user');
 
 function updatePrefs(req,res) {
     console.log('hitting path')
-    User.findByIdAndUpdate(req.params.id, function(err, user) {
+    User.findById(req.user.id, function(err, user) {
+        console.log(req.user.id)
         user.preferences.age = req.body.age;
         user.preferences.size = req.body.size;
         user.preferences.species = req.body.animal;
         user.save();
-    res.render('/', {user:user})
+    res.render('welcome', {user:user})
     });
 }
 
