fdfc53876ffa820d3a23b1cf58845f7b409fc66c
john solis <1johnmichael.solis@gmail.com>
2017-08-15 23:33:42 -0700

    successfully accessed client side points needed to pass to server

--- public/javascripts/results.js
+++ public/javascripts/results.js
@@ -1,27 +1,34 @@
-console.log('loaded results.js');
-
 $(document).ready(function() {
   console.log('jQ ready now');
 
-
-$('button.heart-button').click(function(evt) {
+$('span.boob').click(function(evt) {
   var $this = $(this);
-  if($this.hasClass('heart-button')){
+  var id = $this.attr('id')
+  var userId = $this.data().userid;
+
+  if($this.hasClass('boob')){
     $(this).find('span.glyphicon-heart').css("color", "#FF0000");
-    $this.toggleClass('heart-button');
-    fetch (`api/favorites/${evt.target.id}`)
+    $this.toggleClass('boob');
+    
+    fetch (`api/favorites/${id}`,
+    {
+      method:'GET',
+      credentials: 'include',
+      body: {
+        userId: userId,
+        petId: id
+      },
+      headers: {
+        content:'application/json'
+      }
+    });
+
   } else {
+    console.log('i need to find this pet')
     $(this).find('span.glyphicon-heart').css("color", "#000000");
     $this.toggleClass('heart-button');
   }
 })
-
-
-
-
-
-
-
 
 
 
