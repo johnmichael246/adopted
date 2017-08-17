ecc120701c2381469948cdb5d32aa091c95a10c1
chelsea barocio <barocio.chelsea@gmail.com>
2017-08-16 20:46:36 -0700

    ignore these changes john

--- public/javascripts/results.js
+++ public/javascripts/results.js
@@ -77,5 +77,14 @@ $('span.showFavPet').click(function(evt){
   });
 })
 
+
+
+function formatPhoneNumber(s) {
+  var s2 = (""+s).replace(/\D/g, '');
+  var m = s2.match(/^(\d{3})(\d{3})(\d{4})$/);
+  return (!m) ? null : "(" + m[1] + ") " + m[2] + "-" + m[3];
+}
+
+
 })
 
