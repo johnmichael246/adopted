f6dc3d553c903d92e88b4cf02f59e6960fb4fec0
john solis <1johnmichael.solis@gmail.com>
2017-08-18 00:31:01 -0700

    favoriting features now work on favpets page

--- public/javascripts/results.js
+++ public/javascripts/results.js
@@ -1,77 +1,71 @@
 $(document).ready(function() {
 
-// $('#nextPage').on('click', function (event) {
-//   // console.log('event =', $(event.target).data())
-//   var data = $(event.target).data()
-//   console.log(data.animal, data.size, data.age, data.zip)
-//   // console.log(data-animal, data-size, data-offset, data-age);
-//   fetch ("/pets",
-//     {
-//       method:"POST",
-//       credentials: 'include',
-//       headers: {
-//         content:'application/json'
-//       }
-//     });
-// });
-
-$('span.boob').click(function(evt) {
-  var $this = $(this);
-  var id = $this.attr('id')
-  var userId = $this.data().userid;
-  if ($this.hasClass('red')) {
-    console.log('hes red')
-    $this.removeClass('red').addClass('black')
-    console.log('switched back to red')
-        fetch (`api/favorites/${id}`,
-    {
-      method:'GET',
-      credentials: 'include',
-      body: {
-        userId: userId,
-        petId: id
-      },
-      headers: {
-        content:'application/json'
-      }
-    });
-  } else if ($this.hasClass('black')) {
-    console.log('hes black')
-    $this.removeClass('black').addClass('red')
-    console.log('switched back to red')
-        fetch (`api/favorites/${id}`,
-    {
-      method:'GET',
-      credentials: 'include',
-      body: {
-        userId: userId,
-        petId: id
-      },
-      headers: {
-        content:'application/json'
-      }
-    });   
-  } else if($this.hasClass('boob')){
-    console.log('this pet has not been favorited yet')
-    $(this).css("color", "#FF0000");
-    $this.toggleClass('boob'); 
-    fetch (`api/favorites/${id}`,
-    {
-      method:'GET',
-      credentials: 'include',
-      body: {
-        userId: userId,
-        petId: id
-      },
-      headers: {
-        content:'application/json'
-      }
-    });
-  } else {
-    console.log('this pet has already been favorited')
-    $(this).css("color", "#000000");
-    
-    $this.toggleClass('boob');
+  $('span.boob').click(function(evt) {
+    var $this = $(this);
+    var id = $this.attr('id')
+    var userId = $this.data().userid;
+    if ($this.hasClass('red')) {
+      console.log('hes red')
+      $this.removeClass('red').addClass('black')
+      console.log('switched back to black')
+          fetch (`api/favorites/${id}`,
+      {
+        method:'GET',
+        credentials: 'include',
+        body: {
+          userId: userId,
+          petId: id
+        },
+        headers: {
+          content:'application/json'
+        }
+      });
+    } else if ($this.hasClass('black')) {
+      console.log('hes black')
+      $this.removeClass('black').addClass('red')
+      console.log('switched back to red')
+          fetch (`api/favorites/${id}`,
+      {
+        method:'GET',
+        credentials: 'include',
+        body: {
+          userId: userId,
+          petId: id
+        },
+        headers: {
+          content:'application/json'
+        }
+      });   
+    } else if($this.hasClass('boob')){
+      console.log('this pet has not been favorited yet')
+      $(this).css("color", "#FF0000");
+      $this.toggleClass('boob'); 
+      fetch (`api/favorites/${id}`,
+      {
+        method:'GET',
+        credentials: 'include',
+        body: {
+          userId: userId,
+          petId: id
+        },
+        headers: {
+          content:'application/json'
+        }
+      });
+    } else {
+      console.log('this pet has already been favorited')
+      $(this).css("color", "#000000");
+      
+      $this.toggleClass('boob');
+      fetch(`api/favorites/${id}`, {
+        body: {
+          userId: userId
+        },
+        headers: {
+          content: 'application/json'
+        }
+      });
+    }
     fetch(`api/favorites/${id}`, {
       body: {
         userId: userId
@@ -80,17 +74,7 @@ $('span.boob').click(function(evt) {
         content: 'application/json'
       }
     });
-  }
-
-  fetch(`api/favorites/${id}`, {
-    body: {
-      userId: userId
-    },
-    headers: {
-      content: 'application/json'
-    }
-  });
-})
+  })
 
   function formatPhoneNumber(s) {
     var s2 = (""+s).replace(/\D/g, '');
